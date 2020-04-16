13b9a56b5b024aeb4f3d03adddd6262602adee83
Frédérique Mittelstaedt <git@gfm.io>
2020-04-16 23:30:34 +0100

    remove shelljs dependency

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var shell = require('shelljs');
+var spawnSync = require("child_process").spawnSync;
 
 /** Returns the UID for this username, if it exists, or else undefined */
 function getUidByName(username) {
-  var result = shell.exec(`id -u ${username}`, { silent: true });
+  var result = spawnSync("id", ["-u", username], { shell: true, encoding: "utf8" });
 
-  if (result.code !== 0) {
+  if (result.status !== 0) {
     return undefined;
   }
 
-  var uid = parseInt(result, 10);
+  var uid = parseInt(result.stdout, 10);
 
   if (Number.isNaN(uid)) {
     return undefined;
--- index.mjs
+++ index.mjs
@@ -1,14 +1,14 @@
-import shell from "shelljs";
+import { spawnSync } from "child_process";
 
 /** Returns the UID for this username, if it exists, or else undefined */
 export default function getUidByName(username) {
-  const result = shell.exec(`id -u ${username}`, { silent: true });
+  const result = spawnSync("id", ["-u", username], { shell: true, encoding: "utf8" });
 
-  if (result.code !== 0) {
+  if (result.status !== 0) {
     return undefined;
   }
 
-  const uid = parseInt(result, 10);
+  const uid = parseInt(result.stdout, 10);
 
   if (Number.isNaN(uid)) {
     return undefined;
